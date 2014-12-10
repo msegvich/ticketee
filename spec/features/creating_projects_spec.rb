4f7c3fdead5ddbab9113f8bfac572b71ccd02e7f
Matt Segvich <msegvich@software-craftsmanship.com>
2014-12-10 12:37:27 -0600

    'Create new project' feature complete.

---
+++ spec/features/creating_projects_spec.rb
@@ -0,0 +1,13 @@
+require 'spec/rails_helper'
+
+feature 'Creating Projects' do
+  scenario "can create project" do
+    visit '/'
+    click_link 'New Project'
+    fill_in 'Name', with: 'TextMate 2'
+    fill_in 'Description', with: 'A text-editor for OS X'
+    click_button 'Create Project'
+
+    expect(page).to have_content('Project has been created.')
+  end
+end
