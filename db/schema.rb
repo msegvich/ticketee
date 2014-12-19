8178ba530f3da3ab2443c2767e8ab4a992eb45fb
Matt Segvich <msegvich@software-craftsmanship.com>
2014-12-19 10:37:13 -0600

    Can now create tickets for a project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141210173106) do
+ActiveRecord::Schema.define(version: 20141219160204) do
 
   create_table "projects", force: true do |t|
     t.string   "name",        limit: 255
@@ -19,5 +19,15 @@ ActiveRecord::Schema.define(version: 20141210173106) do
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
   end
+
+  create_table "tickets", force: true do |t|
+    t.string   "title",       limit: 255
+    t.text     "description"
+    t.integer  "project_id"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  add_index "tickets", ["project_id"], name: "index_tickets_on_project_id"
 
 end
